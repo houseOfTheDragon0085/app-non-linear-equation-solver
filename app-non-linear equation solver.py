0b7bb4d9b5820c34d342002e70021182c08f036e
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-03 10:54:08 +0530

    Update app-non-linear equation solver.py

--- app-non-linear equation solver.py
+++ app-non-linear equation solver.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sympy as sym
 from scipy.optimize import fsolve
 import numpy as np
 
@@ -7,19 +8,25 @@ st.title("Nonlinear Equation Solver")
 st.write("Enter a system of equations in the form of F(x, y, w) = 0")
 
 # User input for the equations
-equation1 = st.text_input("Equation 1 (e.g., 'x**2 + y**2 - 20 = 0'):")
-equation2 = st.text_input("Equation 2 (e.g., 'y - x**2 = 0'):")
-equation3 = st.text_input("Equation 3 (e.g., 'w + 5 - x*y = 0'):")
+equation1 = st.text_input("Equation 1 (e.g., 'x**2 + y**2 - 20'):")
+equation2 = st.text_input("Equation 2 (e.g., 'y - x**2'):")
+equation3 = st.text_input("Equation 3 (e.g., 'w + 5 - x*y'):")
+
+# Define symbolic variables
+x, y, w = sym.symbols('x y w')
+
+# Define the system of equations using sympy
+equation1_sym = sym.Eq(sym.sympify(equation1), 0)
+equation2_sym = sym.Eq(sym.sympify(equation2), 0)
+equation3_sym = sym.Eq(sym.sympify(equation3), 0)
 
 def myFunction(z):
-    x = z[0]
-    y = z[1]
-    w = z[2]
+    x, y, w = z
 
     F = np.empty((3))
-    F[0] = eval(equation1)  # Parse and evaluate user input as the first equation
-    F[1] = eval(equation2)  # Parse and evaluate user input as the second equation
-    F[2] = eval(equation3)  # Parse and evaluate user input as the third equation
+    F[0] = equation1_sym.subs({x: x, y: y, w: w}).evalf()
+    F[1] = equation2_sym.subs({x: x, y: y, w: w}).evalf()
+    F[2] = equation3_sym.subs({x: x, y: y, w: w}).evalf()
     return F
 
 zGuess = np.array([1, 1, 1])
